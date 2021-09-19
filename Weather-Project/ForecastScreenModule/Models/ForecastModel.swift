7e9ae517a41e881b6d9f5f34fcddde22ae9029f0
Vlad Artyomkin <artyomkinvlad@yandex.by>
2021-09-19 12:24:32 +0300

    Creating Initial Models (for TodayModule and ForecastModule)

--- Weather-Project/ForecastScreenModule/Models/ForecastModel.swift
+++ Weather-Project/ForecastScreenModule/Models/ForecastModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct ForecastResponseList: Codable {
+    var list: [Forecast]
+    var city: City
+}
+struct Forecast: Codable {
+    var weather: [WeatherForecast]
+    var main: MainForecast
+    var dt_txt: String
+}
+
+struct WeatherForecast: Codable {
+    var main: String
+    var description: String
+    var icon: String
+}
+
+struct MainForecast: Codable {
+    var temp: Float
+    var pressure: Float
+    var humidity: Float
+}
+
+struct City: Codable {
+    var name: String
+}
--- Weather-Project/TodayScreenModule/Models/TodayModel.swift
+++ Weather-Project/TodayScreenModule/Models/TodayModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+struct TodayResponse: Codable {
+    var name: String
+    var weather: [WeatherToday]
+    var main: MainToday
+    var wind: WindToday
+    var clouds: CloudsToday
+    var sys: CountryToday
+}
+
+struct WeatherToday: Codable {
+    var main: String
+    var description: String
+    var icon: String
+}
+
+struct MainToday: Codable {
+    var temp: Float
+    var pressure: Float
+    var humidity: Float
+}
+
+struct WindToday: Codable {
+    var speed: Float
+}
+
+struct CloudsToday: Codable {
+    var all: Float
+}
+
+struct CountryToday: Codable {
+    var country: String
+}
