2e57d66b283594d7884548815c41a0438ad11214
Vlad Artyomkin <artyomkinvlad@yandex.by>
2021-09-19 14:51:04 +0300

    Create window and rootViewController (TabBarController and base settings)

---
+++ Weather-Project/Supporting/MainTabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  MainTabBarController.swift
+//  Weather-Project
+//
+//  Created by Влад Артемкин on 19.09.21.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTabBarController()
+    }
+    
+    private func setTabBarController() {
+        let todayViewController = TodayViewController()
+        let forecastViewController = ForecastViewController()
+        self.setViewControllers([todayViewController, forecastViewController], animated: true)
+        setTabBarItems()
+    }
+    
+    private func setTabBarItems() {
+        let tabBarItem1 = self.tabBar.items![0]
+        tabBarItem1.image = UIImage(systemName: "sun.max")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+        tabBarItem1.selectedImage = UIImage(systemName: "sun.max")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+        tabBarItem1.title = "Today"
+
+        let tabBarItem2 = self.tabBar.items![1]
+        tabBarItem2.image = UIImage(systemName: "cloud.moon.rain")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+        tabBarItem2.selectedImage = UIImage(systemName: "cloud.moon.rain")?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+        tabBarItem2.title = "Forecast"
+    }
+
+}
+
--- Weather-Project/TodayScreenModule/View/TodayViewController.swift
+++ Weather-Project/TodayScreenModule/View/TodayViewController.swift
@@ -11,7 +11,7 @@ class TodayViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = .yellow
         // Do any additional setup after loading the view.
     }
     
